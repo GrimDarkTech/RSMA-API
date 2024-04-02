d5e218b2dba5fcee018147b8b18b108e4d3eaf84
kms0207 <75982005+kms0207@users.noreply.github.com>
2024-04-02 06:38:41 +0400

    #Added functions for commands

--- Python/rsmapy.py
+++ Python/rsmapy.py
@@ -20,7 +20,40 @@ class RSMA:
     def execute(command: str):
         if(RSMA.is_connected):
             RSMA.client.send_message("<|CMD|>" + command)
+
+    def load_scene(scene_name: str):
+        RSMA.execute(f"scene load {scene_name}")
+
+    def add_marker(color: str, x, y, z):
+        RSMA.execute(f"marker {color} {x} {y} {z}")
+
+    def add_wall(start_x, start_y, start_z, end_x, end_y, end_z, height, width):
+        RSMA.execute(f"wall {start_x} {start_y} {start_z} {end_x} {end_y} {end_z} {height} {width}")
+
+    def add_robot(robot_name: str, x, y, z, r_x, r_y, r_z):
+        RSMA.execute(f"robot {robot_name} {x} {y} {z} {r_x} {r_y} {r_z}")
     
+    def gpio_write(id, port, pin, value):
+        RSMA.execute(f"gpio_write {id} {port} {pin} {value}")
+
+    def add_drone(x, y, z):
+        RSMA.execute(f"drone {x} {y} {z}")
+
+    def set_drone_acceleration(id, x, y, z, yaw):
+        RSMA.execute(f"drone_move {id} {x} {y} {z} {yaw}")
+
+    def set_drone_camera(id, x, y, z, smooth):
+        RSMA.execute(f"drone_camera {id} {x} {y} {z} {smooth}")
+
+    def set_drone_control(id, mode):
+        RSMA.execute(f"drone_manual_control {id} {mode}")
+
+    def writer_start(id):
+        RSMA.execute(f"writer_start {id}")
+    
+    def writer_stop(id):
+        RSMA.execute(f"writer_stop {id}")
+
     def message(text: str):
         if(RSMA.is_connected):
             RSMA.client.send_message(text)
